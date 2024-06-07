f051cbe69b7f039a21879fc5758f69abb15c79f5
Abhijeet Jain <abhijeetjain456@gmail.com>
2024-06-07 08:46:42 +0530

    Initial setup

---
+++ src/Functions/Functions.java
@@ -0,0 +1,27 @@
+package Functions;
+
+import java.util.ArrayList;
+
+public class Functions {
+    public static boolean NumberExistsIn1DArray(ArrayList<Integer> arr, int a){
+        for (int j : arr) {
+            if (j == a) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int CountFactors(int N){
+        int count = 0;
+        for(int i = 1; i*i<=N; i++){
+            if(i*i == N){
+                count++;
+            }
+            if(N%i == 0){
+                count+=2;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+import static Functions.Functions.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>(List.of(1, 2, 3, 4, 5));
+        int a1 = 10;
+        int a2 = 4;
+        System.out.println("Number a1 exists in arr?  " + NumberExistsIn1DArray(array, a1));
+        System.out.println("Number a2 exists in arr?  " + NumberExistsIn1DArray(array, a2));
+
+        System.out.println("Number of factors for 10 is: " + CountFactors(10));
+    }
+}
