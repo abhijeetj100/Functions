38d79f306590d281faf3f8457dfec49e0c256a19
Abhijeet Jain <abhijeetjain456@gmail.com>
2024-06-08 14:47:21 +0530

    Feat: Fn(s) for Reversing and Rotating an array
    Reverse(int[] nums, int start, int end)
    Rotate(int[] nums, int k)
    Refactored code
    
    Feat: Fn(s) for Reversing and Rotating an array
    
    Reverse(int[] nums, int start, int end)
    Rotate(int[] nums, int k)
    Refactored code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-import static Functions.Functions.*;
+import static Functions.Arrays.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -38,6 +38,12 @@ public class Main {
 
         int[] nums5 = new int[]{1, 1, 1, 2, 2, 3};
         System.out.println("Majority element in array nums5: " + MajorityElement(nums5));
+        System.out.println();
+
+        int[] nums6 = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int k = 5;
+        System.out.println("Rotated array by k=5: ");
+        Rotate(nums6, k);
         System.out.println();
     }
 }
